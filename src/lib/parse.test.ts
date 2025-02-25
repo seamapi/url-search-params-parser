3308bc4a17af097aab677f50c5ac4d00b376e140
Evan Sosenko <evan@getseam.com>
2025-02-25 00:18:03 -0800

    Add basic parsing implementation

--- src/lib/parse.test.ts
+++ src/lib/parse.test.ts
@@ -3,23 +3,23 @@ import { z } from 'zod'
 
 import { parseUrlSearchParams } from './parse.js'
 
-test.failing('parseUrlSearchParams: with string input', (t) => {
+test('parseUrlSearchParams: with string input', (t) => {
   t.deepEqual(
     parseUrlSearchParams(
       'foo=d&bar=2',
       z.object({ foo: z.string().optional(), bar: z.number().optional() }),
     ),
-    { foo: 'd', bar: 2 },
+    { foo: 'd', bar: '2' },
   )
 })
 
-test.failing('parseUrlSearchParams: with URLSearchParams input', (t) => {
+test('parseUrlSearchParams: with URLSearchParams input', (t) => {
   t.deepEqual(
     parseUrlSearchParams(
       new URLSearchParams('foo=d&bar=2'),
       z.object({ foo: z.string().optional(), bar: z.number().optional() }),
     ),
-    { foo: 'd', bar: 2 },
+    { foo: 'd', bar: '2' },
     'with URLSearchParams input',
   )
 })
--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,10 +1,50 @@
-import type { ZodSchema } from 'zod'
+import type { ZodTypeAny } from 'zod'
+
+import {
+  isZodArray,
+  isZodBoolean,
+  isZodNumber,
+  isZodObject,
+  isZodSchema,
+  isZodString,
+} from './zod.js'
 
 export const parseUrlSearchParams = (
-  _query: URLSearchParams | string,
-  _schema: ZodSchema,
+  query: URLSearchParams | string,
+  schema: ZodTypeAny,
 ): Record<string, unknown> => {
-  // const _searchParams =
-  //   typeof query === 'string' ? new URLSearchParams(query) : query
-  return {}
+  const searchParams =
+    typeof query === 'string' ? new URLSearchParams(query) : query
+
+  if (!isZodObject(schema)) {
+    throw new Error(
+      'The Zod schema to parse URL search params must be an ZodObject schema',
+    )
+  }
+
+  const obj: Record<string, unknown> = {}
+  for (const [k, v] of Object.entries(
+    schema.shape as unknown as Record<string, unknown>,
+  )) {
+    if (searchParams.has(k)) {
+      if (isZodSchema(v)) obj[k] = parse(k, searchParams.getAll(k), v)
+    }
+  }
+
+  return obj
+}
+
+const parse = (k: string, values: string[], schema: ZodTypeAny): unknown => {
+  if (isZodNumber(schema)) return values[0]
+  if (isZodBoolean(schema)) return values[0]
+  if (isZodString(schema)) return values[0]
+  if (isZodArray(schema)) return values
+  throw new UnparseableSearchParamError(k, 'unsupported type')
+}
+
+export class UnparseableSearchParamError extends Error {
+  constructor(name: string, message: string) {
+    super(`Could not parse parameter: '${name}' ${message}`)
+    this.name = this.constructor.name
+  }
 }
